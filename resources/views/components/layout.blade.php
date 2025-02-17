f2facadc3335ce71bfa5f9d59249b8d85011331b
Elyas Damenu <elyasdamenu@gmail.com>
2025-02-17 13:34:18 +0000

    adding the styling of the header for each view.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -80,6 +80,13 @@
           </div>
         </div>
       </nav>
-    {{ $slot }}
+    <header class="bg-white shadow">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm: px-6 lg: py-8">
+            <h1 class="text-3xl font-bold tracking-tight text-grey-900">{{ $heading }}</h1>
+        </div>
+    </header>
+    <main>
+        {{ $slot }}
+    </main>
 </body>
 </html>
