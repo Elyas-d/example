df5cdde777655c7657131b340e7b478cf6864185
Elyas Damenu <elyasdamenu@gmail.com>
2025-02-18 19:49:02 +0000

    updated some styles

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -3,8 +3,8 @@
         Job info
     </x-slot:heading>
     <div class="job-container ml-5">
-        <h2>{{ $job['title'] }}</h2>
-        <p>
+        <h2 class="text-large font-bold ml-3">{{ $job['title'] }}</h2>
+        <p class="ml-5">
             this job pays {{ $job['salary'] }} per year.
         </p>
     </div>
