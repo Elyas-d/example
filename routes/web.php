3881ed3498c99bcd1eb1624ceefec70921632e10
Elyas Damenu <elyasdamenu@gmail.com>
2025-02-21 05:52:58 +0000

    scaling the app

--- routes/web.php
+++ routes/web.php
@@ -2,49 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 
+$jobs = [
+    [
+        'id' => 1,
+        'title' => 'director',
+        'salary' => '$60,000'
+    ],
+    [
+        'id' => 2,
+        'title' => 'programer',
+        'salary' => '$10,000'
+    ],
+    [
+        'id' => 3,
+        'title' => 'teacher',
+        'salary' => '$40,000'
+    ]
+];
+
 Route::get('/', function () {
     return view('home');
 });
-Route::get('/jobs', function () {
-    return view('jobs',[
-        'job'=>[
-            [
-                'id'=>1,
-                'title'=>'director',
-                'salary'=>'$60,000'
-            ],
-            [
-                'id'=>2,
-                'title'=>'programer',
-                'salary'=>'$10,000'
-            ],
-            [
-                'id'=>3,
-                'title'=>'teacher',
-                'salary'=>'$40,000'
-            ]
-        ]
+Route::get('/jobs', function () use($jobs) {
+    return view('jobs', [
+        'job' => $jobs
     ]);
 });
-Route::get('/jobs/{id}', function ($id) {
-    $job=[
-        [
-            'id'=>1,
-            'title'=>'director',
-            'salary'=>'$60,000'
-        ],
-        [
-            'id'=>2,
-            'title'=>'programer',
-            'salary'=>'$10,000'
-        ],
-        [
-            'id'=>3,
-            'title'=>'teacher',
-            'salary'=>'$40,000'
-        ]
-        ];
-    $job=Arr::first($job,fn($job) => $job['id'] == $id);
+Route::get('/jobs/{id}', function ($id) use( $jobs) {
+    $job = $jobs;
+    $job = Arr::first($job, fn($job) => $job['id'] == $id);
     return view('job', ['job' => $job]);
 });
 
