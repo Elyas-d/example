eb59db3d200ac23717406ad4181d6b66a8e5bfd3
Elyas Damenu <elyasdamenu@gmail.com>
2025-02-21 06:01:54 +0000

    created a container for the jobs array

--- routes/web.php
+++ routes/web.php
@@ -2,34 +2,41 @@
 
 use Illuminate\Support\Facades\Route;
 
-$jobs = [
-    [
-        'id' => 1,
-        'title' => 'director',
-        'salary' => '$60,000'
-    ],
-    [
-        'id' => 2,
-        'title' => 'programer',
-        'salary' => '$10,000'
-    ],
-    [
-        'id' => 3,
-        'title' => 'teacher',
-        'salary' => '$40,000'
-    ]
-];
+class job
+{
+    public static function all(): array
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'director',
+                'salary' => '$60,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'programer',
+                'salary' => '$10,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'teacher',
+                'salary' => '$40,000'
+            ]
+        ];
+    }
+}
+
 
 Route::get('/', function () {
     return view('home');
 });
-Route::get('/jobs', function () use($jobs) {
+Route::get('/jobs', function ()  {
     return view('jobs', [
-        'job' => $jobs
+        'job' => job::all()
     ]);
 });
-Route::get('/jobs/{id}', function ($id) use( $jobs) {
-    $job = $jobs;
+Route::get('/jobs/{id}', function ($id) {
+    $job = job::all();
     $job = Arr::first($job, fn($job) => $job['id'] == $id);
     return view('job', ['job' => $job]);
 });
