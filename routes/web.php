f2865a7e25d3c65778695e79fec632b44d175566
Elyas Damenu <elyasdamenu@gmail.com>
2025-03-04 10:06:22 +0000

    multiple update starting from the correction of the create view not showing(updated routes and job model)

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,45 @@
 <?php
 
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
 });
-Route::get('/jobs', function ()  {
-    $jobs = Job::with('employer')->cursorPaginate(10);
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
-});
-
-
-Route::get('/job/create', function () {
-    return view('jobs.create');
-});
-
 
 Route::get('/contact', function () {
     return view('contact');
 });
 
-Route::get('/jobs/{id}', function ($id) { 
-    $job = Job::find($id); 
-    if (! $job) {
-        abort(404);
-    }
-    return view('jobs.show', ['job' => $job]);
+Route::prefix('jobs')->group(function () {
+
+    Route::get('/', function ()  {
+        $jobs = Job::with('employer')->latest()->cursorPaginate(10);
+        return view('jobs.index', [
+            'jobs' => $jobs
+        ]);
+    });
+
+    Route::get('/create', function () {
+        return view('jobs.create');
+    });
+
+    Route::get('/{id}', function ($id) { 
+        $job = Job::find($id); 
+        if (! $job) {
+            abort(404);
+        }
+        return view('jobs.show', ['job' => $job]);
+    });
+
+    Route::post('/', function () {
+        //validate
+        Job::create([
+            'title'=> request('title'),
+            'salary'=> request('salary'),
+            'employer_id'=> 1
+        ]);
+        return redirect('/jobs');
+    });
 });
