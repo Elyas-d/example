9314ce5fd046a2ff91d04540963b89226586dd7f
Elyas Damenu <elyasdamenu@gmail.com>
2025-03-18 10:33:53 +0000

    created another component for the form button

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -29,10 +29,7 @@
 
                 <!-- Submit Button -->
                 <div>
-                    <button type="submit"
-                        class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        Submit
-                    </button>
+                    <x-form_button>submit</x-form_button>
                 </div>
             </form>
         </div>
