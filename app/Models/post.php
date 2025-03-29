85243a02552980cb9693489cdcf26a02007e209d
Elyas Damenu <elyasdamenu@gmail.com>
2025-03-29 19:05:06 +0000

    nothing of importance just an improvement

--- app/Models/post.php
+++ app/Models/post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class post extends Model
 {
@@ -11,6 +12,8 @@ class post extends Model
     use HasFactory;
     protected $fillable = ['post','likes','dislikes','comments'];
 
-    
+    public function comments(){
+        return $this->hasMany(comments::class);
+    }
 
 }
