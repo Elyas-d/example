3b6c1579b732047a53497511492f32e630df4e46
Elyas Damenu <elyasdamenu@gmail.com>
2025-03-30 20:17:36 +0100

    improved the email attachment view

--- resources/views/mail/job-posted.blade.php
+++ resources/views/mail/job-posted.blade.php
@@ -1,3 +1,6 @@
 <h2>{{ $job->title }}</h2>
+<div><p>regarding your job submission </p></div>
 <p>congrats your job is now posted on our website.</p>
+
 <x-button href="{{ url('/job'.$job->id)}}">view your website</x-button>
+<footer><p>2025</p></footer>
