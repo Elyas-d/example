9de9cfb8ed57d350b972df6f699d0d696cc190f4
Elyas Damenu <elyasdamenu@gmail.com>
2025-04-02 15:21:11 +0100

    litterally one letter change for mobile styling mistake

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -75,7 +75,7 @@
             <div class="space-y-1 px-2 pt-2 pb-3">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                 <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
-                <x-nav-link href="/jobs" :active="request()->is('jjobs')">Jobs</x-nav-link>
+                <x-nav-link href="/jobs" :active="request()->is('jobs')">Jobs</x-nav-link>
                 <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
             </div>
         </div>
